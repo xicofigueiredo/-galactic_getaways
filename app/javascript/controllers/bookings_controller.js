011bfda1b2e32eb56ce4277d63dd1b9fe6dcee9f
Francisco Figueiredo <francisco-abf@hotmail.com>
2023-03-02 13:09:38 +0000

    price is finaly working

--- app/javascript/controllers/bookings_controller.js
+++ app/javascript/controllers/bookings_controller.js
@@ -3,14 +3,28 @@ import { Controller } from "@hotwired/stimulus"
 // Connects to data-controller="bookings"
 export default class extends Controller {
 
-  static targets = ["entryDate", "exitDate"]
+  static targets = ["entryDate", "exitDate", "finalPrice"]
   static values = { "price": Number }
 
-  connect() {
-    console.log(this.entryDateTarget)
-    console.log(this.exitDateTarget)
-    console.log(this.priceValue)
-    // const nightCount = Math.ceil((exitDate - entryDate) / (1000 * 60 * 60 * 24));
-    console.dir(nightCount)
+  calculateTotalPrice() {
+    const start = new Date(this.entryDateTarget.value)
+    const end = new Date(this.exitDateTarget.value)
+    const price = this.priceValue
+
+    const nightsMiliseconds = end.getTime() - start.getTime()
+    const nights = nightsMiliseconds / (1000 * 60 * 60 * 24)
+
+    const totalPrice = nights * price
+
+    if (isNaN(start)){
+      this.finalPriceTarget.innerText = `Select a valid Entry date`;
+    } else if (isNaN(end)){
+      this.finalPriceTarget.innerText = `Select a valid Exit date`
+    } else if (nights <= 0) {
+      alert("PLEASE SELECT VALID DATES")
+      this.finalPriceTarget.innerText = `Select valid dates`
+    } else {
+      this.finalPriceTarget.innerText = `Total price: ${totalPrice} €`;
+    }
   }
 }
